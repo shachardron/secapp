46ba20ae80ed4137c8ff46fd26ab45a74f725f2a
shachar.doron@gmail.com <Doron1971!!!>
2022-11-16 21:30:49 +0200

    text on login

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-central-1_wu4N5Ikpw', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6st47i73a82ipkvk5onijo3no0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
